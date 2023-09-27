c0fb9e921c317ca2235203473148e1b7c52524cf
said karim <saidkarim@gmail.com>
2023-09-27 17:20:27 +0100

    add Range Xor Queries file

---
+++ Bootcamp/Range Xor Queries.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    int q;
+    long long *tab;
+    long long *p;
+    
+    cin >> n >> q;
+    
+    tab = new long long[n];
+    for (int i = 0; i < n; i++) {
+        cin >> tab[i];
+    }
+    
+    p = new long long[n];
+    p[0] = tab[0];
+    
+    for (int i = 1; i < n; i++) {
+        p[i] = p[i - 1] ^ tab[i];
+    }
+    
+    while (q-- > 0) {
+        int l;
+	int r;
+
+        cin >> l >> r;
+        l--;
+        r--;
+        
+        cout << (l == 0 ? p[r] : (p[r] ^ p[l - 1])) << endl;
+    }
+    
+    delete[] tab;
+    delete[] p;
+    
+    return 0;
+}
+
