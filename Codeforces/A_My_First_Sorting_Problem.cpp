97e0758dfb66dd8cee67050b07be1259d8dc0f84
SaidKarim-web <sirsaidkarim@gmail.com>
2024-05-10 20:49:04 +0100

    Implement a solution for My First Sorting Problem

---
+++ Codeforces/A_My_First_Sorting_Problem.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int	n;
+
+	cin >> n;
+	for(int i = 0; i < n; i++)
+	{
+		int a,b;
+		cin >> a;
+		cin >> b;
+		if (a < b)
+			cout << a << " " << b << endl;
+		else
+			cout << b << " " << a << endl;
+	}
+}
