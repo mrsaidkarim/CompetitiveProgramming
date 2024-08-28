1ec130d84df24a895f91c0c68ccbdbb661057c46
SaidKarim-web <sirsaidkarim@gmail.com>
2024-08-28 21:17:30 +0100

    Implement a solution for C_Exponentiation

---
+++ BootCamp24/C_Exponentiation.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <unordered_map>
+#include <limits>
+#include <cmath>
+using namespace std;
+
+const long long mod = 1e9+7;
+
+long long my_pow(long long a, long long b)
+{
+	long long ans = 1;
+	while (b)
+	{
+		if (b % 2)
+			ans = ((ans % mod) * a) % mod;
+		b /= 2;
+		a = ((a % mod) * (a % mod)) % mod;
+	}
+	return (ans);
+}
+int main()
+{
+	int n;
+
+	cin >> n;
+	for(int i = 0; i < n; i++)
+	{
+		long long a, b;
+
+		cin >> a >> b;
+		cout << my_pow(a, b) << endl;
+	}
+	return (0);
+}
