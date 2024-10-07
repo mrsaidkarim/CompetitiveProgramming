ca8360c7d53fd584da069e9fb7b1f2c01d508d04
Said Karim <skarim@e3r3p17.1337.ma>
2024-10-07 14:32:41 +0100

    Implement a solution for Minimum_String_Length_After_Removing_Substrings

---
+++ Leetcode/CPP/2696. Minimum String Length After Removing Substrings.cpp
@@ -0,0 +1,44 @@
+
+class Solution {
+public:
+    int minLength(string s) {
+        int check = 1;
+        vector<char> st;
+        int i = 0;
+        while (i < s.length())
+        {
+            if (s[i] == 'A' && i != s.length() - 1 && s[i + 1] == 'B')
+                i+=2;
+            else if (s[i] == 'C' && i != s.length() - 1 && s[i + 1] == 'D')
+                i+=2;
+            else
+            {
+                st.push_back(s[i]);
+                i++;
+            }
+            
+        }
+        while (check)
+        {
+            check = 0;
+            for (int i = 0; i < st.size() - 1 && st.size() != 0; i++)
+            {
+                if (st[i] == 'A' && st[i + 1] == 'B')
+                {
+                    st.erase(st.begin() + i);
+                    st.erase(st.begin() + i);
+                    check = 1;
+                    break;
+                }
+                else if (st[i] == 'C' && st[i + 1] == 'D')
+                {
+                    st.erase(st.begin() + i);
+                    st.erase(st.begin() + i);
+                    check = 1;
+                    break;
+                }
+            }
+        }
+        return (st.size());
+    }
+};
