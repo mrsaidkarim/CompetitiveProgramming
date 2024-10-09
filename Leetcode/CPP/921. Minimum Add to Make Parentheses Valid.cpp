5f843e9b4573813939f1ea1b62f52543c458a8df
Said Karim <sirsaidkarim@gmail.com>
2024-10-09 12:03:41 +0100

    Implement a solution for Minimum_Add_to_Make_Parentheses_Valid

---
+++ Leetcode/CPP/921. Minimum Add to Make Parentheses Valid.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<char> st;
+        for(char c: s)
+        {
+            if (st.empty())
+                st.push(c);
+            else if (c == ')' && st.top() == '(')
+                st.pop();
+            else
+                st.push(c);
+        }
+        return (st.size());
+    }
+};
